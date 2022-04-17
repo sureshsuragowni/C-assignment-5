e2038f2a19226fbed9a1b5ab6e1b5f4d00e96013
sureshsuragowni <sureshgowd08@gmail.com>
2022-04-17 20:02:33 +0530

    commited

---
+++ CustomGeneric Class Mystack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomGenericStackWeek
+{
+    public class MystackWeekdays
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> mystack = new Stack<string>();
+            //Adding Item in stack
+            mystack.Push("Sunday");
+            mystack.Push("Monday");
+            mystack.Push("Tuesday");
+            mystack.Push("Wednesday");
+            mystack.Push("Thursday");
+            mystack.Push("Friday");
+            mystack.Push("Saturday");
+            print(mystack);
+
+            //Accessing Top Item without removing it
+
+            Console.WriteLine("\nTop Item is : {0}", mystack.Peek());
+
+            //Removing Item from Stack
+
+            Console.WriteLine("\nRemoved Top Item of Stack : " + mystack.Pop());
+            Console.WriteLine("\nNow Stack's Items are : ");
+            print(mystack);
+        }
+        public static void print(Stack<string> sp)
+        {
+            foreach (string s in sp)
+            {
+                Console.Write(s.ToString() + " | ");
+            }
+        }
+    }
+}
---
+++ LinkedlistEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LinkedEmployees
+{
+    internal class Employeelist
+    {
+        static void Main(string[] args)
+        {
+            // Creating a linkedlist
+            // Using LinkedList class
+            LinkedList<String> my_list = new LinkedList<String>();
+
+            // Adding elements in the LinkedList
+            // Using AddLast() method
+            my_list.AddLast("Suresh");
+            my_list.AddLast("Shilpa");
+            my_list.AddLast("Rohit");
+            my_list.AddLast("Rohan");
+            my_list.AddLast("Ramesh");
+            my_list.AddLast("Suprith");
+
+            Console.WriteLine("Employee Details...:");
+
+            // Accessing the elements of
+            // LinkedList Using foreach loop
+            foreach (string str in my_list)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Total number of employees in the linked list is : {0} ", my_list.Count);
+        }
+    }
+}
