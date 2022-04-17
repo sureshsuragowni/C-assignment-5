e5bb275d193326d68c091a9b2e9fe809066eaac1
sureshsuragowni <sureshgowd08@gmail.com>
2022-04-17 20:06:55 +0530

    commited

---
+++ playerteam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playerteam
+{
+    internal class Player  
+    {
+        Player[] data = null;
+        public String Name { get; set; }
+        public int Runs { get; set; }
+
+        public Player()
+        {
+            data = new Player[11];
+        }
+        public void Add(Player runs)
+        {
+            data[0] = runs;
+        }
+
+
+    }
+}
